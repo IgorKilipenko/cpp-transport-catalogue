de6bf980a9bc7f895e063025f1e5becaba67b280
IgorKilipenko <igorkili@mail.ru>
2022-08-07 04:13:30 +0700

    start solve sprint10 training: TreeNode (2.2 unique_ptr)

--- transport-catalogue/main.cpp
+++ transport-catalogue/main.cpp
@@ -1,8 +1,25 @@
 #include <iostream>
 
-#include "./training/cats_and_witches.h"
+#include "training/tree.h"
 
 int main() {
-    Test();
+    using namespace std;
+    using T = TreeNode<int>;
+    auto root1 = N(6, N(4, N(3), N(5)), N(7));
+    assert(CheckTreeProperty(root1.get()));
+
+    T* iter = begin(root1.get());
+    while (iter) {
+        cout << iter->value << " "s;
+        iter = next(iter);
+    }
+    cout << endl;
+
+    auto root2 = N(6, N(4, N(3), N(5)), N(7, N(8)));
+    assert(!CheckTreeProperty(root2.get()));
+
+    // Функция DeleteTree не нужна. Узлы дерева будут рекурсивно удалены
+    // благодаря деструкторам unique_ptr
+    
     return 0;
 }
