aba9f5ba41d5500283850b2e4e08d7b76b3d3f53
IgorKilipenko <igorkili@mail.ru>
2022-08-08 04:32:20 +0700

    solve (sedit) sprint10 training: House (1.1 OOP)

--- transport-catalogue/main.cpp
+++ transport-catalogue/main.cpp
@@ -1,11 +1,25 @@
 #include <iostream>
 
-#include "training/cache.h"
+#include "training/house/house.h"
 
 int main() {
-    using namespace trainig::sprint10::cache::tests;
-    Test1();
-    Test2();
+    using namespace trainig::sprint10::house;
 
-    return 0;
+    Resources resources{10000};
+    Builder builder1{resources};
+    Builder builder2{resources};
+
+    House house1 = builder1.BuildHouse(HouseSpecification{12, 9, 3});
+    assert(house1.GetLength() == 12);
+    assert(house1.GetWidth() == 9);
+    assert(house1.GetHeight() == 3);
+    cout << resources.GetBrickCount() << " bricks left"s << endl;
+
+    House house2 = builder2.BuildHouse(HouseSpecification{8, 6, 3});
+    assert(house2.GetLength() == 8);
+    cout << resources.GetBrickCount() << " bricks left"s << endl;
+
+    House banya = builder1.BuildHouse(HouseSpecification{4, 3, 2});
+    assert(banya.GetHeight() == 2);
+    cout << resources.GetBrickCount() << " bricks left"s << endl;
 }
