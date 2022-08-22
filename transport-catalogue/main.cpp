97401b92a48f4e3ad1f57d6f31cb58008141d601
IgorKilipenko <igorkili@mail.ru>
2022-08-22 07:33:42 +0700

    add JsonReaderTester

--- transport-catalogue/main.cpp
+++ transport-catalogue/main.cpp
@@ -4,6 +4,7 @@
 #include <sstream>
 #include <vector>
 
+#include "./tests/json_reader_test.h"
 #include "./tests/json_test.h"
 #include "./tests/svg_test.h"
 #include "./tests/transport_catalogue_test.h"
@@ -22,45 +23,52 @@
  */
 
 int main() {
+    using namespace transport_catalogue;
     using namespace transport_catalogue::tests;
     using namespace svg::tests;
     using namespace json::tests;
+    
+        SvgTester svg_tester;
+        svg_tester.TestSvglib();
 
-    SvgTester svg_tester;
-    svg_tester.TestSvglib();
+        JsonTester json_tester;
+        json_tester.TestJsonlib();
+    
 
-    JsonTester json_tester;
-    json_tester.TestJsonlib();
+    JsonReaderTester json_reader_tester;
+    json_reader_tester.RunTests();
 
-    TransportCatalogueTester tester;
-    tester.TestTransportCatalogue();
+    
+        TransportCatalogueTester tester;
+        tester.TestTransportCatalogue();
 
-    std::shared_ptr<io::RequestHandler> main_request_handler_ptr;
-    std::string json_file = transport_catalogue::detail::io::FileReader::Read(
-        std::filesystem::current_path() /= "transport-catalogue/tests/data/json_requests/test2.json");
-    std::stringstream istream;
-    io::JsonReader json_reader{istream};
-    istream << json_file << std::endl;
-    TransportCatalogue catalog;
-    io::JsonResponseSender stat_sender(std::cerr);
-    io::renderer::MapRenderer renderer;
-    main_request_handler_ptr = std::make_shared<io::RequestHandler>(catalog.GetStatDataReader(), catalog.GetDataWriter(), stat_sender, renderer);
-    json_reader.AddObserver(main_request_handler_ptr);
-    json_reader.ReadDocument();
-    {
+        std::shared_ptr<io::RequestHandler> main_request_handler_ptr;
+        std::string json_file = transport_catalogue::detail::io::FileReader::Read(
+            std::filesystem::current_path() / "transport-catalogue/tests/data/json_requests/test2.json");
+        std::stringstream istream;
+        io::JsonReader json_reader{istream};
+        istream << json_file << std::endl;
         TransportCatalogue catalog;
-        json_file = transport_catalogue::detail::io::FileReader::Read(
-            std::filesystem::current_path() /= "transport-catalogue/tests/data/json_requests/test1.json");
-        istream << json_file;
-        // std::cerr << istream.str() << std::endl;
-        const auto request_handler_ptr = std::make_shared<io::RequestHandler>(catalog.GetStatDataReader(), catalog.GetDataWriter(), stat_sender, renderer);
-        json_reader.AddObserver(request_handler_ptr);
+        io::JsonResponseSender stat_sender(std::cerr);
+        io::renderer::MapRenderer renderer;
+        main_request_handler_ptr = std::make_shared<io::RequestHandler>(catalog.GetStatDataReader(), catalog.GetDataWriter(), stat_sender, renderer);
+        json_reader.AddObserver(main_request_handler_ptr);
         json_reader.ReadDocument();
-    }
-    json_file = transport_catalogue::detail::io::FileReader::Read(
-        std::filesystem::current_path() /= "transport-catalogue/tests/data/json_requests/test3.json");
-    istream << json_file;
-    json_reader.ReadDocument();
-
+        {
+            TransportCatalogue catalog;
+            json_file = transport_catalogue::detail::io::FileReader::Read(
+                std::filesystem::current_path() / "transport-catalogue/tests/data/json_requests/test1.json");
+            istream << json_file;
+            // std::cerr << istream.str() << std::endl;
+            const auto request_handler_ptr =
+                std::make_shared<io::RequestHandler>(catalog.GetStatDataReader(), catalog.GetDataWriter(), stat_sender, renderer);
+            json_reader.AddObserver(request_handler_ptr);
+            json_reader.ReadDocument();
+        }
+        json_file = transport_catalogue::detail::io::FileReader::Read(
+            std::filesystem::current_path() / "transport-catalogue/tests/data/json_requests/test3.json");
+        istream << json_file;
+        json_reader.ReadDocument();
+    
     return 0;
 }
---
+++ transport-catalogue/tests/json_reader_test.h
@@ -0,0 +1,105 @@
+#pragma once
+
+#include <cassert>
+#include <iostream>
+#include <istream>
+#include <iterator>
+#include <numeric>
+#include <ostream>
+#include <sstream>
+#include <string>
+#include <string_view>
+
+#include "../json_reader.h"
+#include "../request_handler.h"
+
+namespace transport_catalogue::tests {
+    using namespace std::literals;
+
+    class JsonReaderTester {
+    private:
+        class MockRequestObserver : public io::IRequestObserver {
+        public:
+            MockRequestObserver(std::ostream& out, std::string name) : out_(out), name_{name} {}
+
+            void OnBaseRequest(std::vector<io::RawRequest>&& requests) const override {
+                out_ << "Has " << requests.size() << " requests" << std::endl;
+                if (requests.empty()) {
+                    return;
+                }
+                out_ << "Base requests:" << std::endl;
+
+                json::Array array;
+                std::for_each(std::make_move_iterator(requests.begin()), std::make_move_iterator(requests.end()), [&array](auto&& req) {
+                    json::Dict dict;
+                    std::for_each(std::make_move_iterator(req.begin()), std::make_move_iterator(req.end()), [&dict](auto&& req_val) {
+                        std::string key = std::move(req_val.first);
+                        json::Node::ValueType val = detail::convertors::VariantCast(std::move(req_val.second));
+
+                        std::visit(
+                            [&key, &dict](auto&& val) {
+                                dict.emplace(std::move(key), std::move(val));
+                            },
+                            std::move(val));
+                    });
+                    array.emplace_back(std::move(dict));
+                });
+                json::Document result(std::move(array));
+                result.Print(out_);
+                out_ << std::endl;
+            }
+
+            /*void OnBaseRequest(const std::vector<io::RawRequest>& requests) const override{
+                std::cerr << "OnBaseRequest" << std::endl;
+            };*/
+            void OnStatRequest(std::vector<io::RawRequest>&& requests) const override {}
+            //!void OnStatRequest(const std::vector<io::RawRequest>& requests) const override {}
+
+            std::string& GetName() {
+                return name_;
+            }
+
+            const std::string& GetName() const {
+                return name_;
+            }
+
+        private:
+            std::ostream& out_;
+            std::string name_;
+        };
+
+    public:
+        void Test() const {
+            const auto print_address = [](const void* ptr) {
+                std::ostringstream oss;
+                oss << ptr;
+                return oss.str();
+            };
+
+            std::stringstream stream;
+            MockRequestObserver observer(std::cerr, "Mock Observer #1");
+            MockRequestObserver observer2(std::cerr, "Mock Observer #2");
+            io::JsonReader json_reader{stream};
+
+            auto subscriber = std::make_shared<MockRequestObserver>(observer);
+            json_reader.AddObserver(subscriber);
+
+            auto subscriber2 = std::make_shared<MockRequestObserver>(observer2);
+            json_reader.AddObserver(subscriber2);
+
+            json::Dict dict{
+                {"base_requests",
+                 json::Array{json::Dict{
+                     {"id", 1}, {"observer address", print_address(static_cast<const void*>(subscriber.get()))}, {"name", observer.GetName()}}}}};
+            json::Document request{dict};
+
+            request.Print(stream);
+
+            json_reader.ReadDocument();
+        }
+
+        void RunTests() const {
+            Test();
+        }
+    };
+}
