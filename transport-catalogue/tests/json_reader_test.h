9dbbda262fd1ccade48bbb732d813c1b0bc33258
IgorKilipenko <igorkili@mail.ru>
2022-08-24 07:18:55 +0700

    fix tests

--- transport-catalogue/tests/json_reader_test.h
+++ transport-catalogue/tests/json_reader_test.h
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <algorithm>
 #include <cassert>
 #include <iostream>
 #include <istream>
@@ -20,7 +21,7 @@ namespace transport_catalogue::tests {
     private:
         class MockRequestObserver : public io::IRequestObserver {
         public:
-            MockRequestObserver(std::ostream& out, std::string name) : out_(out), name_{name} {}
+            MockRequestObserver(std::ostream& out, std::string name) : out_(out), name_{name}, base_requests_{std::make_shared<json::Array>()} {}
 
             void OnBaseRequest(std::vector<io::RawRequest>&& requests) const override {
                 out_ << "Has " << requests.size() << " requests" << std::endl;
@@ -44,21 +45,16 @@ namespace transport_catalogue::tests {
                     });
                     array.emplace_back(std::move(dict));
                 });
-                json::Document result(std::move(array));
-                result.Print(out_);
-                out_ << std::endl;
+
+                std::move(array.begin(), array.end(), std::back_inserter(*base_requests_));
             }
 
-            /*void OnBaseRequest(const std::vector<io::RawRequest>& requests) const override{
-                std::cerr << "OnBaseRequest" << std::endl;
-            };*/
             void OnStatRequest(std::vector<io::RawRequest>&& /*requests*/) const override {
-                std::cerr << "OnStatRequest" << std::endl;
+                out_ << "OnStatRequest" << std::endl;
             }
             void OnRenderSettingsRequest(std::vector<io::RawRequest>&& /*requests*/) const override {
-                std::cerr << "OnRenderSettingsRequest" << std::endl;
+                out_ << "OnRenderSettingsRequest" << std::endl;
             }
-            
 
             std::string& GetName() {
                 return name_;
@@ -68,9 +64,17 @@ namespace transport_catalogue::tests {
                 return name_;
             }
 
+            const json::Array& GetBaseRequests() const {
+                return *base_requests_;
+            }
+            json::Array& GetBaseRequests() {
+                return *base_requests_;
+            }
+
         private:
             std::ostream& out_;
             std::string name_;
+            std::shared_ptr<json::Array> base_requests_;
         };
 
     public:
@@ -82,29 +86,78 @@ namespace transport_catalogue::tests {
             };
 
             std::stringstream stream;
-            MockRequestObserver observer(std::cerr, "Mock Observer #1");
-            MockRequestObserver observer2(std::cerr, "Mock Observer #2");
+            std::ostringstream out;
+            MockRequestObserver observer(out, "Mock Observer #1");
+            MockRequestObserver observer2(out, "Mock Observer #2");
             io::JsonReader json_reader{stream};
 
             auto subscriber = std::make_shared<MockRequestObserver>(observer);
             json_reader.AddObserver(subscriber);
+            assert(json_reader.HasObserver());
 
             auto subscriber2 = std::make_shared<MockRequestObserver>(observer2);
             json_reader.AddObserver(subscriber2);
+            assert(json_reader.HasObserver());
 
             json::Dict dict{
                 {"base_requests",
-                 json::Array{json::Dict{
-                     {"id", 1}, {"observer address", print_address(static_cast<const void*>(subscriber.get()))}, {"name", observer.GetName()}}}}};
+                 json::Array{
+                     json::Dict{
+                         {"id", 1}, {"observer address", print_address(static_cast<const void*>(subscriber.get()))}, {"name", observer.GetName()}},
+                     json::Dict{
+                         {"id", 2}, {"observer address", print_address(static_cast<const void*>(subscriber2.get()))}, {"name", observer2.GetName()}},
+                 }}};
             json::Document request{dict};
 
             request.Print(stream);
 
             json_reader.ReadDocument();
+
+            {
+                json::Array expected = dict["base_requests"].AsArray();
+                json::Array result1 = std::move(observer.GetBaseRequests());
+                json::Array result2 = std::move(observer2.GetBaseRequests());
+
+                assert(result1 == result2 && result1 == expected);
+            }
+
+            {
+                json_reader.RemoveObserver(subscriber2);
+                subscriber2.reset();
+                assert(json_reader.HasObserver());
+
+                request.Print(stream);
+                json_reader.ReadDocument();
+
+                json::Array expected = dict["base_requests"].AsArray();
+                json::Array result1 = std::move(observer.GetBaseRequests());
+                json::Array result2 = std::move(observer2.GetBaseRequests());
+
+                assert(result2.size() == 0);
+                assert(result1.size() && result1 == expected);
+            }
+
+            {
+                subscriber.reset();
+                assert(!json_reader.HasObserver());
+
+                request.Print(stream);
+                json_reader.ReadDocument();
+
+                json::Array result1 = std::move(observer.GetBaseRequests());
+                json::Array result2 = std::move(observer2.GetBaseRequests());
+
+                assert(result1.size() == 0 && result2.size() == 0);
+            }
         }
 
         void RunTests() const {
+            const std::string prefix = "[JsonReader] ";
+
             TestObserver();
+            std::cerr << prefix << "TestObserver : Done." << std::endl;
+
+            std::cerr << std::endl << "All JsonReader Tests : Done." << std::endl << std::endl;
         }
     };
 }
