a7db15854a24f34bdca54617cbf4b7e3cc98d9eb
IgorKilipenko <igorkili@mail.ru>
2022-08-31 22:17:23 +0700

    clear

--- transport-catalogue/domain.h
+++ transport-catalogue/domain.h
@@ -1,17 +1,5 @@
 #pragma once
 
-/*
- * В этом файле вы можете разместить классы/структуры, которые являются частью предметной области (domain)
- * вашего приложения и не зависят от транспортного справочника. Например Автобусные маршруты и Остановки.
- *
- * Их можно было бы разместить и в transport_catalogue.h, однако вынесение их в отдельный
- * заголовочный файл может оказаться полезным, когда дело дойдёт до визуализации карты маршрутов:
- * визуализатор карты (map_renderer) можно будет сделать независящим от транспортного справочника.
- *
- * Если структура вашего приложения не позволяет так сделать, просто оставьте этот файл пустым.
- *
- */
-
 #include <algorithm>
 #include <cassert>
 #include <cstddef>
@@ -146,7 +134,7 @@ namespace transport_catalogue::data /* Db objects (ORM) */ {
     struct Bus {
         std::string name;
         Route route;
-        bool is_roundtrip = false;  //! Need update logic for Response and Request
+        bool is_roundtrip = false;
         Bus() = default;
         template <
             typename String = std::string, typename Route = data::Route,
@@ -358,10 +346,6 @@ namespace transport_catalogue::data /* Database */ {
         const ITransportDataWriter& GetDataWriter() const;
 
         const ITransportDataReader& GetDataReader() const;
-
-        /*explicit operator ITransportDataWriter() const;
-
-        explicit operator ITransportDataReader() const;*/
 
     protected: /* ORM */
         template <typename Stop = data::Stop, detail::EnableIfSame<Stop, data::Stop> = true>
@@ -624,16 +608,6 @@ namespace transport_catalogue::data /* Database implementation */ {
     const ITransportDataReader& Database<Owner>::GetDataReader() const {
         return db_reader_;
     }
-
-    /*template <class Owner>
-    Database<Owner>::operator ITransportDataWriter() const {
-        return db_writer_;
-    }
-
-    template <class Owner>
-    Database<Owner>::operator ITransportDataReader() const {
-        return db_reader_;
-    }*/
 
     template <class Owner>
     void Database<Owner>::LockDatabase() {
