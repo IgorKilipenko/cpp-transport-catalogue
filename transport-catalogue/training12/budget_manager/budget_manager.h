424539b06c391841cb5f96da3b1d9519cab1f191
IgorKilipenko <igorkili@mail.ru>
2022-09-10 14:56:27 +0700

    nfix2

--- transport-catalogue/training12/budget_manager/budget_manager.h
+++ transport-catalogue/training12/budget_manager/budget_manager.h
@@ -18,20 +18,13 @@ namespace budget_manager {
         // разработайте класс BudgetManager
         double ComputeIncome(Date from, Date to) const {
             double result = 0.;
-            // size_t size = Date::ComputeDistance(from, to);
-            /*for (int i = 0; i < size; ++i) {
-                auto ptr = net_incomes_.find(from);
-                result = ptr == net_incomes_.end() ? 0. : result + net_incomes_.at(from);
-                ++from;
-            }*/
-
             auto begin = earnings_before_taxes_.lower_bound(from);
-            std::cerr << "begin: " << begin->first.ToString() << std::endl;
+            //std::cerr << "begin: " << begin->first.ToString() << std::endl;
             auto end = earnings_before_taxes_.upper_bound(to);
-            //!end = end == earnings_before_taxes_.end() ? end : std::next(end);
+
             std::for_each(begin, end, [&result](const auto& ebt_item) {
                 auto& [ebt, earn] = ebt_item.second;
-                result += ebt + earn * (1. - TAX_RATE);
+                result += ebt + earn;
             });
 
             out_stream_ << result << std::endl;
@@ -39,7 +32,7 @@ namespace budget_manager {
         }
 
         void Earn(Date from, Date to, double value) {
-            size_t size = Date::ComputeDistance(from, to)+1ul;
+            size_t size = Date::ComputeDistance(from, to) + 1ul;
             for (int i = 0; i < size; ++i) {
                 Date date = from;
                 earnings_before_taxes_[date].first += value / size;
@@ -50,14 +43,10 @@ namespace budget_manager {
         void PayTax(Date from, Date to) {
             auto begin = earnings_before_taxes_.lower_bound(from);
             auto end = earnings_before_taxes_.upper_bound(to);
-            //end = end == earnings_before_taxes_.end() ? end : std::next(end);
 
-            std::for_each(begin, end, [this](auto& ebt_item) {
+            std::for_each(begin, end, [](auto& ebt_item) {
                 auto& [date, ebt] = ebt_item;
-                double tax_fee = ebt.first * TAX_RATE;
-                tax_payments_[&date] += tax_fee;
-
-                ebt.second += ebt.first;
+                ebt.second = (ebt.first+ebt.second) * (1.-TAX_RATE);
                 ebt.first = 0.;
             });
         }
@@ -65,7 +54,7 @@ namespace budget_manager {
     private:
         std::ostream& out_stream_;
         std::map<Date, std::pair<double, double>> earnings_before_taxes_;
-        std::map<const Date*, double> tax_payments_;
-        std::map<Date, double> net_incomes_;
+        //std::map<const Date*, double> tax_payments_;
+        //std::map<Date, double> net_incomes_;
     };
 }
