aecab38bacfdc3977f03a653c0ac2cbda09c8fd1
IgorKilipenko <igorkili@mail.ru>
2022-09-11 21:26:49 +0700

    solved sprint12: RouteManager [refactoring]

--- transport-catalogue/main.cpp
+++ transport-catalogue/main.cpp
@@ -1,71 +1,62 @@
 #include <algorithm>
-#include <cassert>
-#include <optional>
-#include <string_view>
-#include <utility>
+#include <cmath>
+#include <iostream>
+#include <map>
+#include <set>
+#include <string>
+#include <unordered_map>
 #include <vector>
 
 using namespace std;
 
-// напишите функцию ComputeStatistics, принимающую 5 параметров:
-// два итератора, выходное значение для суммы, суммы квадратов и максимального элемента
-template <typename InputIt, typename OutSum, typename OutSqrSum, typename OutMax>
-void ComputeStatistics(InputIt first, InputIt last, OutSum& out_sum, OutSqrSum& out_sqr_sum, OutMax& out_max) {
-    using Elem = std::decay_t<decltype(*first)>;
-
-    constexpr bool need_sum = !is_same_v<OutSum, const nullopt_t>;
-    constexpr bool need_sq_sum = !is_same_v<OutSqrSum, const nullopt_t>;
-    constexpr bool need_max = !is_same_v<OutMax, const nullopt_t>;
-
-    if constexpr (need_sum) {
-        Elem sum = *first;
-        for (auto i = first + 1; i != last; ++i) {
-            sum += *i;
-        }
-        out_sum = sum;
+class RouteManager {
+public:
+    void AddRoute(int start, int finish) {
+        reachable_lists_[start].emplace(finish);
+        reachable_lists_[finish].emplace(start);
     }
-    if constexpr (need_sq_sum) {
-        Elem sqr_sum = *first * *first;
-        for (auto i = first + 1; i != last; ++i) {
-            sqr_sum += *i * *i;
+    int FindNearestFinish(int start, int finish) const {
+        int result = abs(start - finish);
+        auto ptr = reachable_lists_.find(start);
+        if (ptr == reachable_lists_.end()) {
+            return result;
         }
-        out_sqr_sum = sqr_sum;
-    }
-    if constexpr (need_max) {
-        Elem max = *first;
-        for (auto i = first + 1; i != last; ++i) {
-            max = std::max(*i, max);
+        const auto& reachable_stations = ptr->second;
+        if (!reachable_stations.empty()) {
+            auto it = reachable_stations.lower_bound(finish);
+            if (it == reachable_stations.end()) {
+                result = std::min(result, std::abs(*(--it) - finish));
+            } else if (it == reachable_stations.begin()) {
+                result = std::min(result, std::abs(*it - finish));
+            } else {
+                result = std::min({result, std::abs(*it - finish), std::abs(*(--it) - finish)});
+            }
         }
-        out_max = max;
-    }
-}
 
-struct OnlySum {
-    int value;
+        return result;
+    }
+
+private:
+    std::unordered_map<int, std::set<int>> reachable_lists_;
 };
 
-OnlySum operator+(OnlySum l, OnlySum r) {
-    return {l.value + r.value};
-}
-OnlySum& operator+=(OnlySum& l, OnlySum r) {
-    return l = l + r;
-}
-
 int main() {
-    vector input = {1, 2, 3, 4, 5, 6};
-    int sq_sum;
-    std::optional<int> max;
+    RouteManager routes;
 
-    // Переданы выходные параметры разных типов - std::nullopt_t, int и std::optional<int>
-    ComputeStatistics(input.begin(), input.end(), nullopt, sq_sum, max);
+    int query_count;
+    cin >> query_count;
 
-    assert(sq_sum == 91 && max && *max == 6);
+    for (int query_id = 0; query_id < query_count; ++query_id) {
+        string query_type;
+        cin >> query_type;
+        int start, finish;
+        cin >> start >> finish;
+        if (query_type == "ADD"s) {
+            routes.AddRoute(start, finish);
+        } else if (query_type == "GO"s) {
+            cout << routes.FindNearestFinish(start, finish) << "\n"s;
+        }
+    }
 
-    vector<OnlySum> only_sum_vector = {{100}, {-100}, {20}};
-    OnlySum sum;
-
-    // Поданы значения поддерживающие только суммирование, но запрошена только сумма
-    ComputeStatistics(only_sum_vector.begin(), only_sum_vector.end(), sum, nullopt, nullopt);
-
-    assert(sum.value == 20);
+    cout.flush();
 }
