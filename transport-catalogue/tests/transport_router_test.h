ae9357e27e975f7cc1b17c847f773ba017b87390
IgorKilipenko <igorkili@mail.ru>
2022-09-18 03:36:48 +0700

    fix and add tests

--- transport-catalogue/tests/transport_router_test.h
+++ transport-catalogue/tests/transport_router_test.h
@@ -46,9 +46,9 @@ namespace transport_catalogue::tests {
             return ostream.str();
         }
 
-        void Test1() const {
-            std::string result = ReadDocument(DATA_PATH / "test1.json");
-            std::string expected_str = transport_catalogue::detail::io::FileReader::Read(DATA_PATH / "test1_output.json");
+        void TestFromExample(std::string file_name, std::string answer_suffix = "output") const {
+            std::string result = ReadDocument(DATA_PATH / (file_name + ".json"));
+            std::string expected_str = transport_catalogue::detail::io::FileReader::Read(DATA_PATH / (file_name + "_" + answer_suffix + ".json"));
             assert(!result.empty());
 
             json::Document doc = json::Document::Load(std::stringstream{result});
@@ -61,7 +61,14 @@ namespace transport_catalogue::tests {
             for (auto result_it = response.begin(), expected_it = expected_response.begin(); result_it != response.end();
                  ++result_it, ++expected_it) {
                 if (expected_it->IsMap() && expected_it->AsMap().count("items")) {
-                    if (*result_it != *expected_it) {
+                    json::Dict res_map = result_it->AsMap();
+                    json::Dict expected_map = result_it->AsMap();
+
+                    assert(res_map.count("request_id") && expected_map.count("request_id"));
+                    assert(res_map.count("total_time") && expected_map.count("total_time"));
+
+                    if (res_map != expected_map ||
+                        (res_map.at("request_id") != expected_map.at("request_id") && res_map.at("total_time") != expected_map.at("total_time"))) {
                         std::cerr << "Test result:" << std::endl;
                         result_it->Print(std::cerr);
                         std::cerr << std::endl;
@@ -73,36 +80,39 @@ namespace transport_catalogue::tests {
                     }
                 }
             }
+
+            /*if (response != expected_response) {
+                std::cerr << "Test result:" << std::endl;
+                json::Node(response).Print(std::cerr);
+                std::cerr << std::endl;
+
+                std::cerr << std::endl << "Test expected result:" << std::endl;
+                json::Node(expected_response).Print(std::cerr);
+
+                assert(false);
+            }*/
+        }
+
+        void Test1() const {
+            TestFromExample("test1");
         }
 
         void Test2() const {
-            std::string result = ReadDocument(DATA_PATH / "test2.json");
-            //!std::cerr << result << std::endl;
-            std::string expected_str = transport_catalogue::detail::io::FileReader::Read(DATA_PATH / "test2_output.json");
-            assert(!result.empty());
+            TestFromExample("test2");
+        }
 
-            json::Document doc = json::Document::Load(std::stringstream{result});
-            assert(doc.GetRoot().IsArray());
-            json::Array response = doc.GetRoot().AsArray();
-            json::Array expected_response = json::Node::LoadNode(std::stringstream{expected_str}).AsArray();
+        void Test3() const {
+            TestFromExample("test3");
+        }
 
-            assert(expected_response.size() >= response.size());
+        void Test4() const {
+            TestFromExample("test4");
+        }
 
-            for (auto result_it = response.begin(), expected_it = expected_response.begin(); result_it != response.end();
-                 ++result_it, ++expected_it) {
-                if (expected_it->IsMap() && expected_it->AsMap().count("items")) {
-                    if (*result_it != *expected_it) {
-                        std::cerr << "Test result:" << std::endl;
-                        result_it->Print(std::cerr);
-                        std::cerr << std::endl;
-
-                        std::cerr << std::endl << "Test expected result:" << std::endl;
-                        expected_it->Print(std::cerr);
-
-                        assert(false);
-                    }
-                }
-            }
+        void TestOnRandomData() const {
+            TestFromExample("s12_final_opentest_1", "answer");
+            TestFromExample("s12_final_opentest_2", "answer");
+            TestFromExample("s12_final_opentest_3", "answer");
         }
 
         void RunTests() const {
@@ -113,6 +123,15 @@ namespace transport_catalogue::tests {
 
             Test2();
             std::cerr << prefix << "Test2 : Done." << std::endl;
+
+            Test3();
+            std::cerr << prefix << "Test3 : Done." << std::endl;
+
+            Test4();
+            std::cerr << prefix << "Test4 : Done." << std::endl;
+
+            TestOnRandomData();
+            std::cerr << prefix << "TestOnRandomData : Done." << std::endl;
 
             std::cerr << std::endl << "All TransportRouter Tests : Done." << std::endl << std::endl;
         }
