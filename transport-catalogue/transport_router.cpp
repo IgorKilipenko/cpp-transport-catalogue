3aaa37b4b3cd8c1d3d4e3ab494f0e87fd1ec2b0c
IgorKilipenko <igorkili@mail.ru>
2022-09-22 03:37:34 +0700

    clear

--- transport-catalogue/transport_router.cpp
+++ transport-catalogue/transport_router.cpp
@@ -57,30 +57,21 @@ namespace transport_catalogue::router {
             return;
         }
 
-        // const auto& last_stop_of_route = bus.GetLastStopOfRoute();
-        /*auto last_it = std::find(bus.route.begin(), bus.route.end(), last_stop_of_route);
-        data::Route route{bus.route.begin(), !bus.is_roundtrip ? std::next(last_it) : bus.route.end()};
-        if (!bus.is_roundtrip) {
-            route.insert(route.end(), last_it,  bus.route.end());
-        }*/
-
         const data::Route& route = bus.route;
 
         for (size_t i = 0; i < route.size() - 1ul; ++i) {
             size_t span = 1;
-            double total_travel_time = settings_.bus_wait_time_min;  //! waigth
+            double total_travel_time = settings_.bus_wait_time_min;
             const data::StopRecord& from_stop_ptr = route[i];
             for (size_t j = i + 1; j < route.size(); ++j) {
                 const data::StopRecord& current_stop_ptr = route[j - 1];
                 const data::StopRecord& next_stop_ptr = route[j];
-                if (from_stop_ptr == next_stop_ptr) {  //! Сомнительно!)
+                if (from_stop_ptr == next_stop_ptr) {
                     continue;
                 }
 
                 auto it = db_reader_.GetDistanceBetweenStops(current_stop_ptr, next_stop_ptr);
-                total_travel_time += it.measured_distance / 1000.0 / settings_.bus_velocity_kmh * 60.0 /*+
-                                     (!bus.is_roundtrip && last_stop_of_route == current_stop_ptr ? settings_.bus_wait_time_min : 0.)*/
-                    ;
+                total_travel_time += it.measured_distance / 1000.0 / settings_.bus_velocity_kmh * 60.0;
 
                 auto ege_id = graph_.AddEdge({index_mapper_.GetAt(from_stop_ptr), index_mapper_.GetAt(next_stop_ptr), total_travel_time});
 
@@ -88,7 +79,7 @@ namespace transport_catalogue::router {
                     bus.name,
                     settings_.bus_wait_time_min,
                     total_travel_time - settings_.bus_wait_time_min,
-                    span,  // Number of distances between stops (aka span count)
+                    span,
                     from_stop_ptr->name,
                     next_stop_ptr->name,
                     current_stop_ptr->name};
