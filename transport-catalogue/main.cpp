07d35a6a07168faddfb223b3989f7a41e05d47a0
IgorKilipenko <igorkili@mail.ru>
2023-03-23 23:38:36 +0700

    solved-self sprint13: Undefined Behavior optimizations (1/1)

--- transport-catalogue/main.cpp
+++ transport-catalogue/main.cpp
@@ -4,11 +4,10 @@
 int main() {
     int64_t a, b;
     std::cin >> a >> b;
-    const int64_t min = -10;// std::numeric_limits<int64_t>::min();
-    const int64_t max = 10;//std::numeric_limits<int64_t>::max();
+    const int64_t min = std::numeric_limits<int64_t>::min();
+    const int64_t max = std::numeric_limits<int64_t>::max();
 
-    if ((a > 0 && b > 0 && max - a < b) || 
-        (a < 0 && b < 0 && min - a > b)) {
+    if ((a > 0 && b > 0 && max - a < b) || (a < 0 && b < 0 && min - a > b)) {
         std::cout << "Overflow!" << std::endl;
     } else {
         std::cout << a + b << std::endl;
