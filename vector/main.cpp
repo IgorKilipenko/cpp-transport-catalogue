03a8ad8e455f6712c905acfd6e30466203da40c5
IgorKilipenko <igorkili@mail.ru>
2023-04-17 16:24:48 +0700

    start solve sprint-14 / step-2 / less-3: (Vector своими руками / Размещающий оператор new)

--- vector/main.cpp
+++ vector/main.cpp
@@ -1,109 +1,204 @@
 #include <cassert>
-#include <memory>
-#include <string>
 
-template <typename Func>
-void ApplyToMany(Func func) {}
+#include "optional.h"
 
-template <typename Foo, typename... Args>
-void ApplyToMany(Foo& F, Args&&... args) {
-    (F(std::forward<Args>(args)), ...);
+struct C {
+    C() noexcept {
+        ++def_ctor;
+    }
+    C(const C& /*other*/) noexcept {
+        ++copy_ctor;
+    }
+    C(C&& /*other*/) noexcept {
+        ++move_ctor;
+    }
+    C& operator=(const C& other) noexcept {
+        if (this != &other) {
+            ++copy_assign;
+        }
+        return *this;
+    }
+    C& operator=(C&& /*other*/) noexcept {
+        ++move_assign;
+        return *this;
+    }
+    ~C() {
+        ++dtor;
+    }
+
+    static size_t InstanceCount() {
+        return def_ctor + copy_ctor + move_ctor - dtor;
+    }
+
+    static void Reset() {
+        def_ctor = 0;
+        copy_ctor = 0;
+        move_ctor = 0;
+        copy_assign = 0;
+        move_assign = 0;
+        dtor = 0;
+    }
+
+    inline static size_t def_ctor = 0;
+    inline static size_t copy_ctor = 0;
+    inline static size_t move_ctor = 0;
+    inline static size_t copy_assign = 0;
+    inline static size_t move_assign = 0;
+    inline static size_t dtor = 0;
+};
+
+void TestInitialization() {
+    C::Reset();
+    {
+        Optional<C> o;
+        assert(!o.HasValue());
+        assert(C::InstanceCount() == 0);
+    }
+    assert(C::InstanceCount() == 0);
+
+    C::Reset();
+    {
+        C c;
+        Optional<C> o(c);
+        assert(o.HasValue());
+        assert(C::def_ctor == 1 && C::copy_ctor == 1);
+        assert(C::InstanceCount() == 2);
+    }
+    assert(C::InstanceCount() == 0);
+
+    C::Reset();
+    {
+        C c;
+        Optional<C> o(std::move(c));
+        assert(o.HasValue());
+        assert(C::def_ctor == 1 && C::move_ctor == 1 && C::copy_ctor == 0 && C::copy_assign == 0 && C::move_assign == 0);
+        assert(C::InstanceCount() == 2);
+    }
+    assert(C::InstanceCount() == 0);
+
+    C::Reset();
+    {
+        C c;
+        Optional<C> o1(c);
+        const Optional<C> o2(o1);
+        assert(o1.HasValue());
+        assert(o2.HasValue());
+        assert(C::def_ctor == 1 && C::move_ctor == 0 && C::copy_ctor == 2 && C::copy_assign == 0 && C::move_assign == 0);
+        assert(C::InstanceCount() == 3);
+    }
+    assert(C::InstanceCount() == 0);
+
+    C::Reset();
+    {
+        C c;
+        Optional<C> o1(c);
+        const Optional<C> o2(std::move(o1));
+        assert(C::def_ctor == 1 && C::copy_ctor == 1 && C::move_ctor == 1 && C::copy_assign == 0 && C::move_assign == 0);
+        assert(C::InstanceCount() == 3);
+    }
+    assert(C::InstanceCount() == 0);
 }
 
-void TestSum() {
-    int x;
-    auto lambda = [&x](int y) {
-        x += y;
-    };
-
-    x = 0;
-    ApplyToMany(lambda, 1);
-    assert(x == 1);
-
-    x = 0;
-    ApplyToMany(lambda, 1, 2, 3, 4, 5);
-    assert(x == 15);
+void TestAssignment() {
+    Optional<C> o1;
+    Optional<C> o2;
+    {  // Assign a value to empty
+        C::Reset();
+        C c;
+        o1 = c;
+        assert(C::def_ctor == 1 && C::copy_ctor == 1 && C::dtor == 0);
+    }
+    {  // Assign a non-empty to empty
+        C::Reset();
+        o2 = o1;
+        assert(C::copy_ctor == 1 && C::copy_assign == 0 && C::dtor == 0);
+    }
+    {  // Assign non empty to non-empty
+        C::Reset();
+        o2 = o1;
+        assert(C::copy_ctor == 0 && C::copy_assign == 1 && C::dtor == 0);
+    }
+    {  // Assign empty to non empty
+        C::Reset();
+        Optional<C> empty;
+        o1 = empty;
+        assert(C::copy_ctor == 0 && C::dtor == 1);
+        assert(!o1.HasValue());
+    }
 }
 
-void TestConcatenate() {
+void TestMoveAssignment() {
+    {  // Assign a value to empty
+        Optional<C> o1;
+        C::Reset();
+        C c;
+        o1 = std::move(c);
+        assert(C::def_ctor == 1 && C::move_ctor == 1 && C::dtor == 0);
+    }
+    {  // Assign a non-empty to empty
+        Optional<C> o1;
+        Optional<C> o2{C{}};
+        C::Reset();
+        o1 = std::move(o2);
+        assert(C::move_ctor == 1 && C::move_assign == 0 && C::dtor == 0);
+    }
+    {  // Assign non empty to non-empty
+        Optional<C> o1{C{}};
+        Optional<C> o2{C{}};
+        C::Reset();
+        o2 = std::move(o1);
+        assert(C::copy_ctor == 0 && C::move_assign == 1 && C::dtor == 0);
+    }
+    {  // Assign empty to non empty
+        Optional<C> o1{C{}};
+        C::Reset();
+        Optional<C> empty;
+        o1 = std::move(empty);
+        assert(C::copy_ctor == 0 && C::move_ctor == 0 && C::move_assign == 0 && C::dtor == 1);
+        assert(!o1.HasValue());
+    }
+}
+
+void TestValueAccess() {
     using namespace std::literals;
-    std::string s;
-    auto lambda = [&s](const auto& t) {
-        if (!s.empty()) {
-            s += " ";
+    {
+        Optional<std::string> o;
+        o = "hello"s;
+        assert(o.HasValue());
+        assert(o.Value() == "hello"s);
+        assert(&*o == &o.Value());
+        assert(o->length() == 5);
+    }
+    {
+        try {
+            Optional<int> o;
+            [[maybe_unused]] int v = o.Value();
+            assert(false);
+        } catch (const BadOptionalAccess& /*e*/) {
+        } catch (...) {
+            assert(false);
         }
-        s += t;
-    };
-
-    ApplyToMany(lambda, "cyan"s, "magenta"s, "yellow"s, "black"s);
-    assert(s == "cyan magenta yellow black"s);
+    }
 }
 
-void TestIncrement() {
-    auto increment = [](int& x) {
-        ++x;
-    };
-
-    int a = 0;
-    int b = 3;
-    int c = 43;
-
-    ApplyToMany(increment, a, b, c);
-    assert(a == 1);
-    assert(b == 4);
-    assert(c == 44);
-}
-
-void TestArgumentForwarding() {
-    struct S {
-        int call_count = 0;
-        int i = 0;
-        std::unique_ptr<int> p;
-        void operator()(int i) {
-            this->i = i;
-            ++call_count;
-        }
-        void operator()(std::unique_ptr<int>&& p) {
-            this->p = std::move(p);
-            ++call_count;
-        }
-    };
-
-    S s;
-
-    ApplyToMany(s, 1, std::make_unique<int>(42));
-    assert(s.call_count == 2);
-    assert(s.i == 1);
-    assert(s.p != nullptr && *s.p == 42);
-}
-
-void TestArgumentForwardingToConstFunction() {
-    struct S {
-        mutable int call_count = 0;
-        mutable int i = 0;
-        mutable std::unique_ptr<int> p;
-        void operator()(int i) const {
-            this->i = i;
-            ++call_count;
-        }
-        void operator()(std::unique_ptr<int>&& p) const {
-            this->p = std::move(p);
-            ++call_count;
-        }
-    };
-
-    const S s;
-    ApplyToMany(s, 1, std::make_unique<int>(42));
-    assert(s.call_count == 2);
-    assert(s.i == 1);
-    assert(s.p != nullptr && *s.p == 42);
+void TestReset() {
+    C::Reset();
+    {
+        Optional<C> o{C()};
+        assert(o.HasValue());
+        o.Reset();
+        assert(!o.HasValue());
+    }
 }
 
 int main() {
-    TestSum();
-    TestConcatenate();
-    TestIncrement();
-    TestArgumentForwarding();
-    TestArgumentForwardingToConstFunction();
-    return 0;
+    try {
+        TestInitialization();
+        TestAssignment();
+        TestMoveAssignment();
+        TestValueAccess();
+        TestReset();
+    } catch (...) {
+        assert(false);
+    }
 }
