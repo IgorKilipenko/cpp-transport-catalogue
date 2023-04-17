9885644f61326b4528afbd0fff1e9ea59761d832
IgorKilipenko <igorkili@mail.ru>
2023-04-17 15:55:59 +0700

    solved sprint-14 / step-2 / less-10 / app1/3: (Vector своими руками / Variadic Templates)

--- vector/main.cpp
+++ vector/main.cpp
@@ -5,8 +5,20 @@
 #include <string_view>
 
 /* Напишите вашу реализацию EqualsToOneOf здесь*/
+template <typename... T>
+bool EqualsToOneOf(const T&... values) {
+    if (sizeof...(values) == 1) {
+        return false;
+    }
+}
+
+template <typename T1, typename T2, typename... Ts>
+bool EqualsToOneOf(const T1& first, const T2& second, const Ts&... args) {
+    return (first == second) || EqualsToOneOf(first, args...);
+}
 
 int main() {
+    using namespace std::literals;
     assert(EqualsToOneOf("hello"sv, "hi"s, "hello"s));
     assert(!EqualsToOneOf(1, 10, 2, 3, 6));
     assert(!EqualsToOneOf(8));
