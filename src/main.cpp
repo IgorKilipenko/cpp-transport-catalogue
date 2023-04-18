977e933d9f8b5d8b1cf11956e7a134ffbcae829c
IgorKilipenko <igorkili@mail.ru>
2023-04-18 23:54:02 +0700

    sprint-14/step-1/ pragma pack - плотная упаковка

--- src/main.cpp
+++ src/main.cpp
@@ -1,22 +1,61 @@
 #include <iostream>
+#include <istream>
+#include <sstream>
 
 #include "./tests/vector_test.h"
 
-struct Plate {
-    char c1;
-    int num;
-    char c2;
-    char c3;
-    int region;
+struct Inner {
+    int i;
+    char c;
 };
+
+struct Outer0 {
+    Inner in1;
+    char c;
+    Inner in2;
+};
+
+struct X {
+    std::array<int, 3> i;
+    double d;
+    char c;
+};
+#pragma pack(push, 1)
+struct Plate {
+    uint32_t num:10;
+    uint32_t region:10;
+    uint32_t c1:4;
+    uint32_t c2:4;
+    uint32_t c3:4;
+};
+#pragma pack(pop)
+
+struct Base1 {
+    int i;
+    char c;
+};
+
+struct Base2 : Base1 {
+    bool b;
+    float f;
+};
+
+struct Outer : Base2 {
+    short sh;
+};
+
+template <typename... Args>
+void print(Args... args) {
+    std::stringstream ss;
+    ((ss << sizeof(args) << ", "), ...);
+    std::string str(ss.str());
+    std::cout << (str.ends_with(", ") ? str.replace(str.end()-2, str.end(), 1, '\0') : str) << std::endl;
+}
 
 int main() {
     using namespace std::literals;
-    std::cout << "Sizeof = "s << sizeof(Plate) << std::endl;
-    std::cout << offsetof(Plate, c1) << std::endl;
-    std::cout << offsetof(Plate, num) << std::endl;
-    std::cout << offsetof(Plate, c2) << std::endl;
-    std::cout << offsetof(Plate, c3) << std::endl;
-    std::cout << offsetof(Plate, region) << std::endl;
+    // std::cout << sizeof(Plate) << " " << offsetof(Plate, c1) << " " << offsetof(Plate, c1) << std::endl;
+    const Plate p{};
+    print(p, p.c1, p.c2, p.c3, p.num, p.region);
     return 0;
 }
