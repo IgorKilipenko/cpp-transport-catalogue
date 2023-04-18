f0c5199fae2a46fc91d2e8cf6b47648d3250a058
IgorKilipenko <igorkili@mail.ru>
2023-04-18 20:02:35 +0700

    sprint-14/step-1/ offset macros

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,22 @@
 #include <iostream>
+
 #include "./tests/vector_test.h"
 
+struct Plate {
+    char c1;
+    int num;
+    char c2;
+    char c3;
+    int region;
+};
+
 int main() {
-    tests::run();
+    using namespace std::literals;
+    std::cout << "Sizeof = "s << sizeof(Plate) << std::endl;
+    std::cout << offsetof(Plate, c1) << std::endl;
+    std::cout << offsetof(Plate, num) << std::endl;
+    std::cout << offsetof(Plate, c2) << std::endl;
+    std::cout << offsetof(Plate, c3) << std::endl;
+    std::cout << offsetof(Plate, region) << std::endl;
     return 0;
 }
