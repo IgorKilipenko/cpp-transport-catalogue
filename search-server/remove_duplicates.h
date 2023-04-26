cafb1d7d070d1883f323466980d9be04dc939f1f
IgorKilipenko <igorkili@mail.ru>
2023-04-26 11:22:43 +0700

    start dev sprint-15 transport-catalogue task-1

--- search-server/remove_duplicates.h
+++
@@ -1,6 +0,0 @@
-#include "search_server.h"
-
-/// Remove all documents duplicates from database
-inline void RemoveDuplicates(SearchServer& search_server) {
-    search_server.RemoveDuplicates();
-}
