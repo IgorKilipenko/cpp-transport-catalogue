730d502d1847e90d07138367e756d240f98d1533
IgorKilipenko <igorkili@mail.ru>
2023-04-26 16:57:29 +0700

    add proto data schema

--- transport-catalogue/main.cpp
+++ transport-catalogue/main.cpp
@@ -15,8 +15,11 @@
 #include "tests/request_handler_test.h"
 #include "tests/transport_router_test.h"
 #include "transport_catalogue.h"
+#include "transport_catalogue.pb.h"
 
-int main() {
+using namespace std::literals;
+
+int tests() {
     using namespace transport_catalogue;
     using namespace transport_catalogue::tests;
     using namespace svg::tests;
@@ -70,5 +73,54 @@ int main() {
 
         svg_map.Render(std::cout);
     */
+
+    tc_data_schema::Coordinates c;
+    c.set_lat(1);
+    c.set_lng(2);
+
+    std::cout << "lat: " << c.lat() << " lng: " << c.lng() << std::endl;
+
     return 0;
+}
+
+void ProcessRequests() {
+    using namespace transport_catalogue;
+    using namespace transport_catalogue::io;
+
+    TransportCatalogue catalog;
+    JsonReader json_reader(std::cin);
+    JsonResponseSender stat_sender(std::cout);
+
+    maps::MapRenderer renderer;
+
+    const auto request_handler_ptr = std::make_shared<RequestHandler>(catalog.GetStatDataReader(), catalog.GetDataWriter(), stat_sender, renderer);
+    json_reader.AddObserver(request_handler_ptr);
+    json_reader.ReadDocument();
+}
+
+void PrintUsage(std::ostream& stream = std::cerr) {
+    stream << "Usage: transport_catalogue [make_base|process_requests]\n"sv;
+}
+
+int main(int argc, char* argv[]) {
+    if (argc != 2) {
+        PrintUsage();
+        return 1;
+    }
+
+    const std::string_view mode(argv[1]);
+
+    if (mode == "make_base"sv) {
+        // make base here
+
+    } else if (mode == "process_requests"sv) {
+        ProcessRequests();
+
+    } else if (mode == "tests"sv) {
+        tests();
+
+    } else {
+        PrintUsage();
+        return 1;
+    }
 }
