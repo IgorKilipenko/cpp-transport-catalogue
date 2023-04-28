4b4647dfa2cdb17ca40a9c4f2ce02a03c791fff9
IgorKilipenko <igorkili@mail.ru>
2023-04-28 13:56:27 +0700

    add Test2 to MakeDatabaseTester

--- transport-catalogue/tests/make_database_test.h
+++ transport-catalogue/tests/make_database_test.h
@@ -28,8 +28,8 @@ namespace transport_catalogue::tests {
 
             maps::MapRenderer renderer;
 
-            const auto request_handler_ptr = std::make_shared<RequestHandler>(
-                catalog.GetStatDataReader(), catalog.GetDataWriter(), stat_sender, renderer, mode);
+            const auto request_handler_ptr =
+                std::make_shared<RequestHandler>(catalog.GetStatDataReader(), catalog.GetDataWriter(), stat_sender, renderer, mode);
             json_reader.AddObserver(request_handler_ptr);
 
             json_reader.ReadDocument();
@@ -44,25 +44,51 @@ namespace transport_catalogue::tests {
         void TestFromFile(std::string file_name, std::string answer_suffix = "expected_res") const {
             std::string result = ReadDocument(DATA_PATH / (file_name + ".json"), io::RequestHandler::Mode::MAKE_BASE);
             std::string request_result = ReadDocument(DATA_PATH / (file_name + "_request" + ".json"), io::RequestHandler::Mode::PROCESS_REQUESTS);
-            std::string expected_result = transport_catalogue::detail::io::FileReader::Read(DATA_PATH / (file_name + "_" + answer_suffix + ".json"));
+            std::string expected_result_str = transport_catalogue::detail::io::FileReader::Read(DATA_PATH / (file_name + "_" + answer_suffix + ".json"));
             assert(result.empty() && !request_result.empty());
+
+            json::Document doc = json::Document::Load(std::stringstream{request_result});
+            assert(doc.GetRoot().IsArray());
+            json::Array response = doc.GetRoot().AsArray();
+            json::Array expected_response = json::Node::LoadNode(std::stringstream{expected_result_str}).AsArray();
+
+            for (auto result_it = response.begin(), expected_it = expected_response.begin(); result_it != response.end();
+                 ++result_it, ++expected_it) {
+                if (expected_it->IsMap() && expected_it->AsMap().count("items")) {
+                    json::Dict res_map = result_it->AsMap();
+                    json::Dict expected_map = expected_it->AsMap();
+
+
+                    if (res_map != expected_map) {
+                        if (res_map.at("request_id") != expected_map.at("request_id")) {
+                            std::cerr << "Test result:" << std::endl;
+                            result_it->Print(std::cerr);
+                            std::cerr << std::endl;
+
+                            std::cerr << std::endl << "Test expected result:" << std::endl;
+                            expected_it->Print(std::cerr);
+                            std::cerr << std::endl;
+                        }
+                    }
+                }
+            }
         }
 
-        void Test1() const {
+        void TestBase() const {
             TestFromExample("step1_test1");
         }
 
-        void Test2() const {
+        void Test1() const {
             TestFromFile("step1_test1");
+        }
+
+        void Test2() const {
+            TestFromFile("step1_test2");
         }
 
         /*
         void Test3() const {
-            TestFromExample("test3");
-        }
-
-        void Test4() const {
-            TestFromExample("test4");
+            TestFromFile("step1_test3");
         }
 
         void TestOnRandomData() const {
@@ -74,6 +100,9 @@ namespace transport_catalogue::tests {
         void RunTests() const {
             const std::string prefix = "[MakeDatabase] ";
 
+            TestBase();
+            std::cerr << prefix << "TestBase : Done." << std::endl;
+
             Test1();
             std::cerr << prefix << "Test1 : Done." << std::endl;
 
@@ -83,7 +112,7 @@ namespace transport_catalogue::tests {
             /*
             Test3();
             std::cerr << prefix << "Test3 : Done." << std::endl;
-
+            
             Test4();
             std::cerr << prefix << "Test4 : Done." << std::endl;
 
